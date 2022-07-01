66864ed4fce6023ad1673c785447d1cb939e7e39
Masashi Sode <masashi.sode@gmail.com>
2022-07-01 17:58:42 +0900

    added type annotation for the example

--- experiments/exp_measurement_timer.py
+++ experiments/exp_measurement_timer.py
@@ -12,7 +12,7 @@ class MeasurementTimer:
     def __init__(self, luxmeter):
         self._luxmeter = luxmeter
 
-    def measure_periodically(self, period: float = 1, end_time: int = 10):
+    def measure_periodically(self, period: float = 1, end_time: int = 10) -> pd.DataFrame:
         if period < 1:
             raise ValueError("Period must be greater than or equal to 1")
 
@@ -30,7 +30,7 @@ class MeasurementTimer:
 
         return df_output
 
-    def _run_measure_thread(self, output_list):
+    def _run_measure_thread(self, output_list: List):
         job_thread = threading.Thread(
             target=self._perform_measurement, kwargs={"output_list": output_list}
         )
@@ -38,7 +38,7 @@ class MeasurementTimer:
 
     # pylint: disable=invalid-name
     # the names ev, y, z are used in the documentation
-    def _perform_measurement(self, output_list):
+    def _perform_measurement(self, output_list: List):
         ev, x, y, measured_time = self._luxmeter.get_ev_x_y()
         output_dict = {"measured_time": str(measured_time), "ev": ev, "x": x, "y": y}
         print(output_dict)
